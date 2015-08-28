a182d52c120a4dae11bf69e24654052fcd024238
unknown <nzsiddiqui@gmail.com>
2015-08-28 22:06:50 +0500

    sugguest slider plus other css and template fixes

---
+++ skin/frontend/ecart/default/js/scriptshome.js
@@ -0,0 +1,8 @@
+jQuery(document).ready(function($) {
+     //jQuery('.selectpicker').selectpicker();
+ }
+ (function() {
+     jQuery(window).load(function() {
+          jQuery("a[rel='m_PageScroll2id']").mPageScroll2id();
+     });
+})(jQuery);
