9b2ac1989684e0447fb77170a56a35ee0bbf90d7
unknown <chkashif167@hotmail.com>
2015-09-10 10:49:44 +0500

    done

--- app/code/local/Tabs/Extension/Block/Category.php
+++ app/code/local/Tabs/Extension/Block/Category.php
@@ -95,8 +95,8 @@ class Tabs_Extension_Block_Category extends Mage_Catalog_Block_Product_Abstract 
         
         // if Category filter is on
         if ($catId) {
-            echo $collection->getSelect()->where('c.parent_id = ?', $catId);
-            exit;
+            $collection->getSelect()->where('c.parent_id = ?', $catId);
+    
         }
 
         // unfortunately I cound not come up with the sql query that could grab only 1 bestseller for each category
