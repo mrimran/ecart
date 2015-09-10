785e5edf620e20895f863bd97fe8dddffd2dfc40
unknown <chkashif167@hotmail.com>
2015-09-10 16:38:16 +0500

    done

--- app/code/local/Tabs/Extension/Block/Perfume.php
+++ app/code/local/Tabs/Extension/Block/Perfume.php
@@ -8,7 +8,7 @@ class Tabs_Extension_Block_Perfume extends Mage_Catalog_Block_Product_Abstract {
     public function getLoadedProductCollection()
     { 
 
-      $id = 65;
+      $id = 8;
        // benchmarking
         $memory = memory_get_usage();
         $time = microtime();
