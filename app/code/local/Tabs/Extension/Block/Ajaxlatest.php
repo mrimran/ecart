f2394a072e032d2a6bb0f2dd2d26130e4c058ea6
unknown <chkashif167@hotmail.com>
2015-09-10 11:42:22 +0500

    done

--- app/code/local/Tabs/Extension/Block/Ajaxlatest.php
+++ app/code/local/Tabs/Extension/Block/Ajaxlatest.php
@@ -55,8 +55,6 @@ class Tabs_Extension_Block_Ajaxlatest extends Mage_Catalog_Block_Product_Abstrac
      */
    protected function getProductCollectionLatest($category)
     {
- 
-       $_category = Mage::getModel('catalog/category')->load($category);
        
        if (is_null($this->_productCollection)) {
             $layer = $this->getLayer();
@@ -89,7 +87,9 @@ class Tabs_Extension_Block_Ajaxlatest extends Mage_Catalog_Block_Product_Abstrac
             }
             /* @var $layer Mage_Catalog_Model_Layer */
             /* @var $layer Mage_Catalog_Model_Layer */
-        $this->_productCollection = $layer->getProductCollection();
+       //$this->_productCollection = $layer->getProductCollection();
+       $id = $this->getRequest()->getParam('id');
+       $_category = Mage::getModel('catalog/category')->load($id);
        $this->_productCollection = Mage::getResourceModel('catalog/product_collection')
        ->addCategoryFilter($_category)
        ->addAttributeToFilter('upcomingproduct', 0)
