180a55992e5bcc8afe9dea38d7b762a1e9914407
Imran <mrimranzahoor@gmail.com>
2015-09-15 18:01:37 +0500

    Removing sort code in layered navigation.

--- app/code/local/Mage/Catalog/Model/Layer/Filter/Abstract.php
+++ app/code/local/Mage/Catalog/Model/Layer/Filter/Abstract.php
@@ -119,7 +119,7 @@ abstract class Mage_Catalog_Model_Layer_Filter_Abstract extends Varien_Object
         if (is_null($this->_items)) {
             $this->_initItems();
         }
-        asort($this->_items);
+        //asort($this->_items);
         return $this->_items;
     }
 
