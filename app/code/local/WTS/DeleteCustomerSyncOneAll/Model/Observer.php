6e0e98fa41a4697cd2d813dc632110db06cc9c05
Imran <mrimranzahoor@gmail.com>
2015-10-06 17:16:35 +0500

    Final changes for introduced observer for social login.

--- app/code/local/WTS/DeleteCustomerSyncOneAll/Model/Observer.php
+++ app/code/local/WTS/DeleteCustomerSyncOneAll/Model/Observer.php
@@ -14,9 +14,10 @@ class WTS_DeleteCustomerSyncOneAll_Model_Observer
         Mage::log("+++"."Now deleting the customer entry (#$custId) for social login in WTS_DeleteCustomerSyncOneAll_Model_Observer.");
 
         //delete user from social login as well.
-        $model = Mage::getModel('oneall_sociallogin/entity');
         try {
-            $model->setId($custId)->delete();
+            $model = Mage::getModel('oneall_sociallogin/entity')->getCollection()->addFieldToFilter('customer_id',array('eq'=>$custId));
+            $model->walk('delete');
+            Mage::log("Deleted entry from social login via WTS_DeleteCustomerSyncOneAll_Model_Observer.");
         } catch (Exception $e) {
             //throw Mage::throwException($e->getMessage());//no need to show any message, just log it.
             Mage::logException("ERROR:".$e->getMessage()." in WTS_DeleteCustomerSyncOneAll_Model_Observer for cust_id:".$custId);
