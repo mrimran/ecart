ead9037faabd9ddd910b021c1e78d7d23b53d6ab
nzsiddiqui <nzsiddiqui@gmail.com>
2015-10-14 16:18:34 +0500

    fixes

--- skin/frontend/ecart/default/js/scriptshome.js
+++ skin/frontend/ecart/default/js/scriptshome.js
@@ -2,9 +2,9 @@
 //      //jQuery('.selectpicker').selectpicker();
 // });
 
-(function() {
-     jQuery(window).load(function() {
-          jQuery("a[rel='m_PageScroll2id']").mPageScroll2id(); 
-		  jQuery("a[rel='m_PageScroll2id_1']").mPageScroll2id({offset:130});
-     });
+(function($){
+$(window).load(function(){
+	jQuery("a[rel='m_PageScroll2id']").mPageScroll2id();
+	jQuery("a[rel='m_PageScroll2id_1']").mPageScroll2id({offset:130});
+});
 })(jQuery);
