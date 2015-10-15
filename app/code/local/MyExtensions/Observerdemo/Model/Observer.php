90c97ac7ef0f83b3230f4aaac2c0e2ce11282c18
uzyre <uzairmalik011@gmail.com>
2015-10-15 08:11:35 -0700

    extension

--- app/code/local/MyExtensions/Observerdemo/Model/Observer.php
+++ app/code/local/MyExtensions/Observerdemo/Model/Observer.php
@@ -3,23 +3,35 @@
 class MyExtensions_Observerdemo_Model_Observer {
  
     public function addtocartEvent(Varien_Event_Observer $observer) {
- echo 'asdf'; exit;
-        $event = $observer->getEvent();  //Gets the event
-        $product = $event->getProduct();
-        $observermsg = "The event triggered>>> <B>" . $event->getName() . "</B><br/> The added product>>> <B> " . $product->getName()."</B>";
-        //Adds the message to the shopping cart
-        echo Mage::getSingleton('checkout/session')->addSuccess($observermsg);
-    }
-     
-    public function autoApproveReview(Varien_Event_Observer $observer)
-    {
-        $event = $observer->getEvent();
-        $reviewObjData = $event->getData();
-        //print_r($reviewObjData);exit;
- 
-        $reviewData=$reviewObjData["data_object"];
-        $reviewData->setStatusId(Mage_Review_Model_Review::STATUS_APPROVED);
-         
-        echo Mage::getSingleton('core/session')->addSuccess("Thank you for your input!!");
+$sku = $observer->getEvent()->getQuoteItem()->getProduct()->getData('sku');
+        $_product = Mage::getModel('catalog/product')->loadByAttribute('sku', $sku);
+
+        $new_price = $_product->getPrice();
+        //$productId = $observer->getEvent()->getQuoteItem()->getProductId();
+
+        /* $item = $observer->getQuoteItem();
+          $item->setCustomPrice($new_price);
+          $item->setOriginalCustomPrice($new_price);
+          $item->setFinalPrice($new_price);
+          $item->getProduct()->setIsSuperMode(true); */
+
+        // Get the quote item
+        $item = $observer->getQuoteItem();
+        // Ensure we have the parent item, if it has one
+        $item = ( $item->getParentItem() ? $item->getParentItem() : $item );
+        // Load the custom price
+        $price = $new_price;
+        // Set the custom price
+        
+        //Set price by subtracting base price from the passed price and 
+        //then adding the product price to tackle any extra price attached with custom options :)
+        //identify if there is some extra cost, final price - $new_price or price
+        $extra_price = $item->getProduct()->getFinalPrice() - $price;//add this extra price
+        $extra_price = ($extra_price > 0) ? $extra_price : 0;
+        echo "final price:".$item->getProduct()->getFinalPrice().", price:".$item->getProduct()->getPrice().", Passed:".($extra_price + $price); die();
+        $item->setCustomPrice($extra_price + $price);
+        $item->setOriginalCustomPrice($extra_price + $price);
+        // Enable super mode on the product.
+        $item->getProduct()->setIsSuperMode(true);
     }
 }
