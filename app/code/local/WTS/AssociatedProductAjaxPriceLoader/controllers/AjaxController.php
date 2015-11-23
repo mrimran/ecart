b76e793d336535d3e2de978886d23184f724b0f0
Imran <mrimranzahoor@gmail.com>
2015-11-23 18:01:10 +0500

    Changes for to handle zero price rate in ajax response.#2

--- app/code/local/WTS/AssociatedProductAjaxPriceLoader/controllers/AjaxController.php
+++ app/code/local/WTS/AssociatedProductAjaxPriceLoader/controllers/AjaxController.php
@@ -42,12 +42,12 @@ class WTS_AssociatedProductAjaxPriceLoader_AjaxController extends Mage_Core_Cont
         }
         $this->getResponse()->setHeader('Content-type', 'application/json');
         if($finalPrice <= 0.00) {
-            $this->getResponse()->setBody("");
-            return;
+            $this->getResponse()->setBody("{}");
+        }else {
+            $finalPrice = Mage::app()->getStore()->getCurrentCurrencyCode() . number_format($finalPrice, 2, '.', ',');//change in this format 1234.54
+            $priceArray = array('price' => $finalPrice);
+            $this->getResponse()->setBody(json_encode($priceArray));
         }
-        $finalPrice = Mage::app()->getStore()->getCurrentCurrencyCode() . number_format($finalPrice, 2, '.', ',');//change in this format 1234.54
-        $priceArray = array('price' => $finalPrice);
-        $this->getResponse()->setBody(json_encode($priceArray));
     }
 
 }
