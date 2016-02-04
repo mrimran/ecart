d8dbb751f2a2cd275604268b0a0e53a04670e4c1
Imran <mrimranzahoor@gmail.com>
2016-02-04 18:45:32 +0500

    Any changes.

--- app/code/local/WTS/AssociatedProductAjaxPriceLoader/Model/Observer.php
+++ app/code/local/WTS/AssociatedProductAjaxPriceLoader/Model/Observer.php
@@ -8,11 +8,26 @@ class WTS_AssociatedProductAjaxPriceLoader_Model_Observer
         $sku = $observer->getEvent()->getQuoteItem()->getProduct()->getData('sku');
         $_product = Mage::getModel('catalog/product')->loadByAttribute('sku', $sku);
         $new_price = $_product->getPrice();
-
+        $new_final_price = $_product->getFinalPrice();
+        $hasSpecialPrice = $this->productHasSpecialPrice($_product);
+        if($hasSpecialPrice) {
+            //don't do anything in case of special price
+            return;
+        }
         // Get the quote item
         $item = $observer->getQuoteItem();
+        $hasParent = false;
+        $parentItem = null;
+        if($item->getParentItem()) {
+            $hasParent = true;
+            $parentItem = $item->getParentItem();
+        }
+        //echo $new_price."|".$new_final_price;
         // Ensure we have the parent item, if it has one
-        $item = ( $item->getParentItem() ? $item->getParentItem() : $item );
+        $item = ( $parentItem ? $parentItem : $item );
+        //echo "SSS:".$specialprice."|".$specialPriceFromDate."|".$specialPriceToDate."<br>";
+        $extra_price = $item->getProduct()->getFinalPrice() - $item->getProduct()->getPrice(); //add this extra price
+        //echo ">>>".$extra_price.">>>".$item->getProduct()->getPrice().">>>".$item->getProduct()->getFinalPrice();die();
         //apply the payment logic on in configurable products only
         if($item->getProduct()->isConfigurable()) {
             // Load the custom price
@@ -32,12 +47,32 @@ class WTS_AssociatedProductAjaxPriceLoader_Model_Observer
                 $item->setOriginalCustomPrice($extra_price + $price);
             }else {*/
                 //if the attributes are defined, use the parent product price instead (base product price)
-                $item->setCustomPrice($item->getProduct()->getFinalPrice());
-                $item->setOriginalCustomPrice($item->getProduct()->getFinalPrice());
+                $item->setCustomPrice($new_final_price);
+                $item->setOriginalCustomPrice($new_final_price);
             //}
             // Enable super mode on the product.
             $item->getProduct()->setIsSuperMode(true);
         }
+    }
+
+    public function productHasSpecialPrice($_product)
+    {
+        $hasSpecialPrice = false;
+        // Get the Special Price
+        $specialprice = $_product->getSpecialPrice();
+        // Get the Special Price FROM date
+        $specialPriceFromDate = $_product->getSpecialFromDate();
+        // Get the Special Price TO date
+        $specialPriceToDate = $_product->getSpecialToDate();
+        // Get Current date
+        $today = time();
+
+        if ($specialprice) {
+            if ($today >= strtotime($specialPriceFromDate) && $today <= strtotime($specialPriceToDate) || $today >= strtotime($specialPriceFromDate) && is_null($specialPriceToDate)) {
+                $hasSpecialPrice = true;
+            }
+        }
+        return $hasSpecialPrice;
     }
 
 }
